dbc2443f95640653ab05e338bf71324cc8c36872
Sam Wellander <samwellander@gmail.com>
2025-05-11 10:45:33 -0700

    make nodes occassionally skip price reports

--- packages/hardhat/scripts/runOracleBots.ts
+++ packages/hardhat/scripts/runOracleBots.ts
@@ -1,6 +1,13 @@
 import { ethers } from "hardhat";
 import { Contract } from "ethers";
 
+const PROBABILITY_OF_PRICE_REPORT = 0.8;
+
+const getRandomPrice = () => {
+  // Random price between 1990 and 2010
+  return Math.floor(Math.random() * 21) + 1990;
+};
+
 const validateNodes = async () => {
   const signers = await ethers.getSigners();
   const oracleAdminAccount = signers[0];
@@ -9,21 +16,22 @@ const validateNodes = async () => {
   await oracle.validateNodes();
 };
 
-const getRandomPrice = () => {
-  // Random price between 1990 and 2010
-  return Math.floor(Math.random() * 21) + 1990;
-};
-
 const reportPrices = async () => {
   const signers = await ethers.getSigners();
   const oracleNodeAccounts = signers.slice(1, 11);
   try {
     await Promise.all(
       oracleNodeAccounts.map(async account => {
-        const oracle = await ethers.getContract<Contract>("StakeBasedOracle", account.address);
-        const price = getRandomPrice();
-        console.log(`Reporting price ${price} from ${account.address}`);
-        return oracle.reportPrice(price);
+        const shouldReport = Math.random() < PROBABILITY_OF_PRICE_REPORT;
+        if (shouldReport) {
+          const oracle = await ethers.getContract<Contract>("StakeBasedOracle", account.address);
+          const price = getRandomPrice();
+          console.log(`Reporting price ${price} from ${account.address}`);
+          return oracle.reportPrice(price);
+        } else {
+          console.log(`Skipping price report from ${account.address}`);
+          return Promise.resolve();
+        }
       }),
     );
   } catch (error) {
@@ -34,11 +42,11 @@ const reportPrices = async () => {
 const run = async () => {
   // Initial price reports
   await reportPrices();
-  
+
   // Set up intervals
   setInterval(async () => {
     await reportPrices();
-  }, 2000);
+  }, 1000);
 
   setInterval(async () => {
     await validateNodes();
