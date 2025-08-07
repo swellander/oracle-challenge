15cfbc2533679351671670ac250d15509ec0530f
Sam Wellander <samwellander@gmail.com>
2025-08-07 12:34:33 -0700

    add price widget to whitelist UI

--- packages/nextjs/app/whitelist/page.tsx
+++ packages/nextjs/app/whitelist/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { NextPage } from "next";
+import { PriceWidget } from "~~/components/oracle/PriceWidget";
 import { WhitelistTable } from "~~/components/oracle/whitelist/WhitelistTable";
 
 const Home: NextPage = () => {
@@ -9,6 +10,9 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5 w-full max-w-5xl mx-auto">
           <div className="flex flex-col gap-8">
+            <div className="w-full">
+              <PriceWidget contractName="WhitelistOracle" />
+            </div>
             <div className="w-full">
               <WhitelistTable />
             </div>
